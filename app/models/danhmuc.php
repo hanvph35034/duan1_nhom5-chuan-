c36bb759fec98151f80e8568bcc6d3e8950785b0
hanvph35034 <sonyeuthunhem@gmail.com>
2023-11-16 23:06:50 +0700

    15/11

--- app/models/danhmuc.php
+++ app/models/danhmuc.php
@@ -1,7 +1,7 @@
 <?php
 // load danh muc
 function loadall_danhmuc(){
-    $sql = "select * from danhmuc where trang_thai = '0' order by id desc";
+    $sql = "SELECT `id`, `ten`, `trang_thai` FROM `danhmuc` WHERE 1";
     $listdanhmuc = pdo_query($sql);
     return $listdanhmuc;
 }
