2a41b7eb9cd9155ae2cca7a6c47edc4beb08fced
hanvph35034 <sonyeuthunhem@gmail.com>
2023-11-26 23:13:01 +0700

    25/11

--- app/views/Client/header.php
+++ app/views/Client/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Autima Dựng án của nhóm 5</title>
+    <title>Autima Dự án của nhóm 5</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon -->
