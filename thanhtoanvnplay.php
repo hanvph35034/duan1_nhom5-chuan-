238b5e61f30e51e25a353f744d09048b823d167f
ngocchi <chimtnph42272@fpt.edu.vn>
2023-12-13 14:41:02 +0700

    13/12

--- thanhtoanvnplay.php
+++ thanhtoanvnplay.php
@@ -1,55 +1,52 @@
 <?php
 
-//Sample Code -- please change the autoload yourself as appropriate
-include_once '../loader.php';
-include_once '../vendor/autoload.php';
+namespace MService\Payment\AllInOne\Processors;
 
-use MService\Payment\AllInOne\Processors\CaptureIPN;
-use MService\Payment\AllInOne\Processors\PayATM;
-use MService\Payment\AllInOne\Processors\QueryStatusTransaction;
-use MService\Payment\AllInOne\Processors\RefundATM;
-use MService\Payment\AllInOne\Processors\RefundMoMo;
-use MService\Payment\AllInOne\Processors\RefundStatus;
+use MService\Payment\AllInOne\Models\CaptureIPNRequest;
 use MService\Payment\Shared\SharedModels\Environment;
 use MService\Payment\Shared\SharedModels\PartnerInfo;
-use MService\Payment\AllInOne\Processors\CaptureMoMo;
+use PHPUnit\Framework\TestCase;
 
-$orderId = time() . "";
-$requestId = time() . "";
+class CaptureIPNTest extends TestCase
+{
 
-$env = new Environment("https://test-payment.momo.vn/gw_payment/transactionProcessor",
-    new PartnerInfo("mTCKt9W3eU1m39TW", 'MOMOLRJZ20181206', 'KqBEecvaJf1nULnhPF5htpG3AMtDIOlD'),
-    'development', '', false);
+    public function test__construct()
+    {
+        $env = new Environment("teehee", new PartnerInfo("mTCKt9W3eU1m39TW", 'MOMOIQA420180417', 'PPuDXq1KowPT1ftR8DvlQTHhC03aul17'),
+            'testing');
+        $captureIPN = new CaptureIPN($env);
 
-/** MoMo Wallet Processes:
- * CaptureMoMo returns a payURL with a QR Code for user to scan with MoMo App and pay
- * QueryStatusTransaction checks the status of the transaction, given orderId and requestId
- * CaptureIPN processes the request MoMo sent to the server
- * RefundMoMo allows user to request refund for transactions paid through MoMo Wallet
- * RefundStatus checks the status of the refund requests, given the orderId of the paid transaction and requestId
- **/
+        $this->assertInstanceOf(Environment::class, $captureIPN->getEnvironment(), "Wrong Data Type for QRNotify Environment");
+        $this->assertInstanceOf(PartnerInfo::class, $captureIPN->getPartnerInfo(), "Wrong Data Type for QRNotify PartnerInfo");
 
-CaptureMoMo::process($env, $orderId, "Pay With MoMo", "35000", "sjygdvi", $requestId, "https://google.com.vn", "https://google.com.vn");
-QueryStatusTransaction::process($env, $requestId, $orderId);
+        $this->assertEquals("teehee", $captureIPN->getEnvironment()->getMoMoEndpoint(), "Wrong MoMoEndpoint in QRNotify SetUp");
+        $this->assertEquals($env->getTarget(), $captureIPN->getEnvironment()->getTarget(), "Wrong MoMoEndpoint in QRNotify SetUp");
+    }
 
-$data = "partnerCode=MOMOLRJZ20181206&accessKey=mTCKt9W3eU1m39TW&requestId=1555383430&orderId=1555383430&orderInfo=&orderType=momo_wallet&transId=2302586804&errorCode=0&message=Success&localMessage=Th%C3%A0nh%20c%C3%B4ng&payType=qr&responseTime=2019-04-09%2014%3A53%3A38&extraData=&signature=e9469360fe360e8c63a97a755300e4321648a796593e41411bea5c426af33249&amount=300000";
-CaptureIPN::process($env, $data);
+    public function testMoMoRequestCorrect()
+    {
+        $env = new Environment("https://test-payment.momo.vn", new PartnerInfo("ZjF6taKUohp7iN8l", 'MOMOTUEN20190312', 'KqBEecvaJf1nULnhPF5htpG3AMtDIOlD'),
+            'development');
+        $data = "partnerCode=MOMOTUEN20190312&accessKey=ZjF6taKUohp7iN8l&requestId=1555383430&orderId=1555383430&orderInfo=&orderType=momo_wallet&transId=2302586804&errorCode=0&message=Success&localMessage=Th%C3%A0nh%20c%C3%B4ng&payType=qr&responseTime=2019-04-09%2014%3A53%3A38&extraData=&signature=2a23a88aab6b6dd00b07669d84904778cc9d429c6a5748fa77298b05886a8620&amount=300000";
+        $captureIPN = new CaptureIPN($env);
 
-$orderId = (time() + (10 * 24 * 60 * 60))."";
-RefundMoMo::process($env, $orderId, '1561972963', '7000', '2304963974');
-RefundStatus::process($env, '1561972963', '1561972963');
+        $request = $captureIPN->getIPNInformationFromMoMo($data);
+        $this->assertInstanceOf(CaptureIPNRequest::class, $request, "Wrong Verification Process for Capture MoMo IPN");
 
-/** MoMo ATM Processes:
- * PayATM
- * QueryStatusTransaction
- * RefundATM
-**/
+        $this->assertEquals('2302586804', $request->getTransId(), "Wrong TransId during creattion of IPNRequest");
+        $this->assertEquals('1555383430', $request->getRequestId(), "Wrong RequestId during creattion of IPNRequest");
+        $this->assertEquals(0, $request->getErrorCode(), "Wrong ErrorCode during creattion of IPNRequest");
+    }
 
-$orderId = (time() + (7 * 24 * 60 * 60))."";
-$requestId = (time() + (7 * 24 * 60 * 60))."";
+    public function testMoMoRequestWrong()
+    {
+        $env = new Environment("https://test-payment.momo.vn", new PartnerInfo("ZjF6taKUohp7iN8l", 'MOMOTUEN20190312', 'KqBEecvaJf1nULnhPF5htpG3AMtDIOlD'),
+            'development');
+        $data = "partnerCode=MOMO&accessKey=ZjF6taKUohp7iN8l&requestId=1555383430&orderId=1555383430&orderInfo=&orderType=momo_wallet&transId=2302586804&errorCode=0&message=Success&localMessage=Th%C3%A0nh%20c%C3%B4ng&payType=qr&responseTime=2019-04-09%2014%3A53%3A38&extraData=&signature=2a23a88aab6b6dd00b07669d84904778cc9d429c6a5748fa77298b05886a8620&amount=300000";
+        $captureIPN = new CaptureIPN($env);
 
-PayATM::process($env, $orderId, "Pay With MoMo", "35000", '', $requestId, "https://google.com.vn", "https://google.com.vn", "SML");
-QueryStatusTransaction::process($env, $orderId, $requestId);
+        $request = $captureIPN->getIPNInformationFromMoMo($data);
+        $this->assertNotInstanceOf(CaptureIPNRequest::class, $request, "Wrong Verification Process for Capture MoMo IPN");
+    }
 
-$orderId = (time() + (5 * 24 * 60 * 60))."";
-RefundATM::process($env, $orderId, '1562059843', '10000', '2304992176', 'SML');
+}
